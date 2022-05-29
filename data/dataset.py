82b591b790f30b830b6e0d855150a0bae06ffcd1
electrobullet <box.for.what@gmail.com>
2022-05-29 09:47:52 +0300

    Create subset csv files from fer2013new.csv

--- data/dataset.py
+++ data/dataset.py
@@ -4,6 +4,19 @@ import numpy as np
 import pandas as pd
 from PIL import Image
 
+LABELS = [
+    'neutral',
+    'happiness',
+    'surprise',
+    'sadness',
+    'anger',
+    'disgust',
+    'fear',
+    'contempt',
+    'unknown',
+    'NF',
+]
+
 
 def unpack(base_folder: str, fer_path: str, ferplus_path: str) -> None:
     folder_aliases = {
@@ -19,17 +32,23 @@ def unpack(base_folder: str, fer_path: str, ferplus_path: str) -> None:
 
     dataframe = pd.concat(
         [
-            pd.read_csv(ferplus_path)[['Usage', 'Image name']],
+            pd.read_csv(ferplus_path),
             pd.read_csv(fer_path)['pixels'],
         ],
         axis=1,
     ).dropna()
 
-    for usage, image_name, pixels in dataframe.values:
+    for key, value in folder_aliases.items():
+        subset = dataframe[dataframe['Usage'] == key]
+        subset = subset.rename(columns={'Image name': 'filename'})
+        subset = subset[['filename', *LABELS]]
+        subset.to_csv(os.path.join(base_folder, '..', f'{value}.csv'), index=False)
+
+    for usage, image_name, pixels in dataframe[['Usage', 'Image name', 'pixels']].values:
         image = Image.fromarray(np.fromstring(pixels, np.uint8, 48 * 48, ' ').reshape(48, 48))
         image_path = os.path.join(base_folder, folder_aliases[usage], image_name)
         image.save(image_path, compress_level=0)
 
 
 if __name__ == '__main__':
-    # unpack('data/pd_images', 'data/fer2013.csv', 'data/fer2013new.csv')
+    unpack('data/images', 'data/fer2013.csv', 'data/fer2013new.csv')
