c01905379f3e0cdddeb73f640d18aefc4e6cc97f
electrobullet <box.for.what@gmail.com>
2022-05-29 11:29:08 +0300

    Add `majority` option to dataset.py

--- data/dataset.py
+++ data/dataset.py
@@ -1,3 +1,4 @@
+import argparse
 import os
 
 import numpy as np
@@ -33,7 +34,7 @@ def unpack(base_folder: str, fer_path: str, ferplus_path: str) -> None:
     dataframe = pd.concat(
         [
             pd.read_csv(ferplus_path),
-            pd.read_csv(fer_path)['pixels'],
+            pd.read_csv(fer_path, usecols=['pixels']),
         ],
         axis=1,
     ).dropna()
@@ -50,5 +51,42 @@ def unpack(base_folder: str, fer_path: str, ferplus_path: str) -> None:
         image.save(image_path, compress_level=0)
 
 
+def majority(file_path: str):
+    dataframe = pd.read_csv(file_path)
+
+    emotion_votes = dataframe[[*LABELS]]
+    emotion_votes = emotion_votes.replace(1, 0)
+
+    labels = []
+
+    for line in emotion_votes.values:
+        if np.max(line) > 0.5 * np.sum(line):
+            labels.append(LABELS[np.argmax(line)])
+        else:
+            labels.append(np.nan)
+
+    dataframe['class'] = labels
+    dataframe = dataframe.dropna()
+    dataframe = dataframe[(dataframe['class'] != 'unknown') & (dataframe['class'] != 'NF')]
+    dataframe[['filename', 'class']].to_csv(f'majority_{os.path.basename(file_path)}', index=False)
+
+
 if __name__ == '__main__':
-    unpack('data/images', 'data/fer2013.csv', 'data/fer2013new.csv')
+    parser = argparse.ArgumentParser()
+    subparsers = parser.add_subparsers(dest='mode')
+
+    parser_unpack = subparsers.add_parser('unpack')
+    parser_unpack.add_argument('-d', '--base_folder', type=str, default='data/images')
+    parser_unpack.add_argument('-fer', '--fer_path', type=str, default='data/fer2013.csv')
+    parser_unpack.add_argument('-ferplus', '--ferplus_path', type=str, default='data/fer2013new.csv')
+
+    parser_majority = subparsers.add_parser('majority')
+    parser_majority.add_argument('-f', '--file_path', type=str, required=True)
+
+    args = parser.parse_args()
+
+    if args.mode == 'unpack':
+        unpack(args.base_folder, args.fer_path, args.ferplus_path)
+
+    elif args.mode == 'majority':
+        majority(args.file_path)
