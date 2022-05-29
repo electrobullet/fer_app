262f84475458a0f56d6e743e8f206b1eccce3682
electrobullet <box.for.what@gmail.com>
2022-05-29 11:43:22 +0300

    Add `info` option to dataset.py

--- data/dataset.py
+++ data/dataset.py
@@ -1,5 +1,6 @@
 import argparse
 import os
+import sys
 
 import numpy as np
 import pandas as pd
@@ -51,8 +52,10 @@ def unpack(base_folder: str, fer_path: str, ferplus_path: str) -> None:
         image.save(image_path, compress_level=0)
 
 
-def majority(file_path: str):
+def majority(file_path: str) -> str:
     dataframe = pd.read_csv(file_path)
+
+    save_path = os.path.join(os.path.dirname(sys.argv[0]), f'majority_{os.path.basename(file_path)}')
 
     emotion_votes = dataframe[[*LABELS]]
     emotion_votes = emotion_votes.replace(1, 0)
@@ -68,7 +71,21 @@ def majority(file_path: str):
     dataframe['class'] = labels
     dataframe = dataframe.dropna()
     dataframe = dataframe[(dataframe['class'] != 'unknown') & (dataframe['class'] != 'NF')]
-    dataframe[['filename', 'class']].to_csv(f'majority_{os.path.basename(file_path)}', index=False)
+    dataframe[['filename', 'class']].to_csv(save_path, index=False)
+
+    return save_path
+
+
+def info(file_path: str) -> None:
+    dataframe = pd.read_csv(file_path)
+
+    total = len(dataframe.index)
+
+    for label in LABELS:
+        n = len(dataframe[dataframe['class'] == label].index)
+        print(f'{label}: {n} ({n / total * 100:.2f}%)')
+
+    print(f'\ntotal: {total}')
 
 
 if __name__ == '__main__':
@@ -83,10 +100,16 @@ if __name__ == '__main__':
     parser_majority = subparsers.add_parser('majority')
     parser_majority.add_argument('-f', '--file_path', type=str, required=True)
 
+    parser_info = subparsers.add_parser('info')
+    parser_info.add_argument('-f', '--file_path', type=str, required=True)
+
     args = parser.parse_args()
 
     if args.mode == 'unpack':
         unpack(args.base_folder, args.fer_path, args.ferplus_path)
 
     elif args.mode == 'majority':
-        majority(args.file_path)
+        info(majority(args.file_path))
+
+    elif args.mode == 'info':
+        info(args.file_path)
